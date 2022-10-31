fecb31424bb087c6cf02b6e75585cc0c185658ad
VinhTien <tien.tran.1612@gmail.com>
2022-10-31 15:11:18 +0700

    update to HCMUT location in forcast print result in newsapi

--- src/forcastIO/forecastIO.py
+++ src/forcastIO/forecastIO.py
@@ -3,31 +3,44 @@ import apikey
 
 api_key = apikey.API_KEY
 
-Lisbon = [38.7252993, -9.1500364]
+BKUni = [10.77305970058611, 106.65934269345048]
 
 fio = ForecastIO.ForecastIO(api_key,
                             units=ForecastIO.ForecastIO.UNITS_SI,
                             lang=ForecastIO.ForecastIO.LANG_ENGLISH,
-                            latitude=Lisbon[0], longitude=Lisbon[1])
+                            latitude=BKUni[0], longitude=BKUni[1])
                             
-print('Latitude', fio.latitude, 'Longitude', fio.longitude)
-print ('Timezone', fio.timezone, 'Offset', fio.offset)
-print(fio.get_url()) # You might want to see the request url
-if fio.has_hourly() is True:
-	hourly = FIOHourly.FIOHourly(fio)
-	print('Hourly')
-	print('Summary:', hourly.summary)
-	print('Icon:', hourly.icon)
-	print
-	for hour in range(0, hourly.hours()):
-		print('Hour', hour+1)
-		for item in hourly.get_hour(hour).keys():
-			print(item + ' : ' + str(hourly.get_hour(hour)[item]))
-		print
-		# Or access attributes directly for a given minute. 
-		# hourly.hour_5_time would also work
-		print(hourly.hour_3_time)
-		print
-	print
+# print('Latitude', fio.latitude, 'Longitude', fio.longitude)
+# print ('Timezone', fio.timezone, 'Offset', fio.offset)
+# print(fio.get_url()) # You might want to see the request url
+# if fio.has_hourly() is True:
+# 	hourly = FIOHourly.FIOHourly(fio)
+# 	print('Hourly')
+# 	print('Summary:', hourly.summary)
+# 	print('Icon:', hourly.icon)
+# 	print()
+# 	for hour in range(0, hourly.hours()):
+# 		print('Hour', hour+1)
+# 		for item in hourly.get_hour(hour).keys():
+# 			print(item + ' : ' + str(hourly.get_hour(hour)[item]))
+# 		print
+# 		# Or access attributes directly for a given minute. 
+# 		# hourly.hour_5_time would also work
+# 		print(hourly.hour_3_time)
+# 		print()
+# 	print()
+# else:
+# 	print('No Hourly data')
+
+if fio.has_currently() is True:
+	currently = FIOCurrently.FIOCurrently(fio)
+	print('Currently')
+	for item in currently.get().keys():
+		print(item + ' : ' + str(currently.get()[item]))
+	print()
+	# Or access attributes directly
+	print('current temperature: ' + str(currently.temperature))
+	print('current humidity; ' + str(currently.humidity))
+	print()
 else:
-	print('No Hourly data')
+	print('No Currently data')
--- src/news-api/newsapi.py
+++ src/news-api/newsapi.py
@@ -1,13 +1,24 @@
+import json
+from turtle import title
 import requests
+import pandas as pd
 
 url = ('https://newsapi.org/v2/top-headlines?'
        'sources=bbc-news&'
        'apiKey=4f8b83aec5a847eb9fbd29815900104f')
 response = requests.get(url)
 
-print(response.json())
-
-
+# print(response.json())
+articles={}
+articles=response.json()
+# print(type(article))
+# article=json.loads(response.json())
+# print(article[title])
+# for article in articles:
+#     print (article)
+#     for y in articles[article]:
+#         print (y,':',article[article][y])
+print(json.dumps(articles, indent=4))
 
 # import pandas as pd
 # apikey = '4f8b83aec5a847eb9fbd29815900104f'
