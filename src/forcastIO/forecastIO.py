8b0e3a6a4933631eab467b6670b94f1cc67753e4
APAC\TIT5HC <fixed-term.Tien.TranVinh@vn.bosch.com>
2022-10-31 17:04:21 +0700

    1. create main 2. create forecastIO class 3. create location class

--- src/forcastIO/forecastIO.py
+++ src/forcastIO/forecastIO.py
@@ -1,15 +1,14 @@
 from forecastiopy import *
-import apikey
+from . import apikey
+import environtment_variable
 
-api_key = apikey.API_KEY
+BKUni = environtment_variable.BKUni
 
-BKUni = [10.77305970058611, 106.65934269345048]
+# fio = ForecastIO.ForecastIO(api_key,
+#                             units=ForecastIO.ForecastIO.UNITS_SI,
+#                             lang=ForecastIO.ForecastIO.LANG_ENGLISH,
+#                             latitude=BKUni[0], longitude=BKUni[1])
 
-fio = ForecastIO.ForecastIO(api_key,
-                            units=ForecastIO.ForecastIO.UNITS_SI,
-                            lang=ForecastIO.ForecastIO.LANG_ENGLISH,
-                            latitude=BKUni[0], longitude=BKUni[1])
-                            
 # print('Latitude', fio.latitude, 'Longitude', fio.longitude)
 # print ('Timezone', fio.timezone, 'Offset', fio.offset)
 # print(fio.get_url()) # You might want to see the request url
@@ -24,7 +23,7 @@ fio = ForecastIO.ForecastIO(api_key,
 # 		for item in hourly.get_hour(hour).keys():
 # 			print(item + ' : ' + str(hourly.get_hour(hour)[item]))
 # 		print
-# 		# Or access attributes directly for a given minute. 
+# 		# Or access attributes directly for a given minute.
 # 		# hourly.hour_5_time would also work
 # 		print(hourly.hour_3_time)
 # 		print()
@@ -32,15 +31,46 @@ fio = ForecastIO.ForecastIO(api_key,
 # else:
 # 	print('No Hourly data')
 
-if fio.has_currently() is True:
-	currently = FIOCurrently.FIOCurrently(fio)
-	print('Currently')
-	for item in currently.get().keys():
-		print(item + ' : ' + str(currently.get()[item]))
-	print()
-	# Or access attributes directly
-	print('current temperature: ' + str(currently.temperature))
-	print('current humidity; ' + str(currently.humidity))
-	print()
-else:
-	print('No Currently data')
+# if fio.has_currently() is True:
+#     currently = FIOCurrently.FIOCurrently(fio)
+#     print('Currently')
+#     for item in currently.get().keys():
+#         print(item + ' : ' + str(currently.get()[item]))
+#     print()
+#     # Or access attributes directly
+#     print('current temperature: ' + str(currently.temperature))
+#     print('current humidity; ' + str(currently.humidity))
+#     print()
+# else:
+#     print('No Currently data')
+
+
+class Weather:
+	def __init__(self, lat, long) -> None:
+		self.api_key = apikey.API_KEY
+		self.location = [lat, long]
+		self.temperature=0
+		self.humidity=0
+
+	def create_fio(self):
+		self.fio = ForecastIO.ForecastIO(self.api_key, units=ForecastIO.ForecastIO.UNITS_SI,
+						lang=ForecastIO.ForecastIO.LANG_ENGLISH, 
+						latitude=self.location[0], 
+						longitude=self.location[1])
+
+
+	def get_current_weatherforecast(self):
+		if self.fio.has_currently() is True:
+			currently = FIOCurrently.FIOCurrently(self.fio)
+			print('Currently')
+			for item in currently.get().keys():
+				print(item + ' : ' + str(currently.get()[item]))
+			print()
+        	# Or access attributes directly
+			self.temperature = currently.temperature
+			self.humidity  = currently.humidity
+			print('current temperature: ' + str(currently.temperature))
+			print('current humidity; ' + str(currently.humidity))
+			print()
+		else:
+			print('No Currently data')
--- src/location/location.py
+++ src/location/location.py
@@ -1,3 +1,22 @@
 import geocoder
-g = geocoder.ip('me')
-print(g.latlng)
+# g = geocoder.ip('me')
+# print(g.latlng)
+
+class UserLocation:
+    def __init__(self) -> None:
+        self.location_instance = geocoder.ip('me')
+        self.user_location = self.location_instance.latlng
+        print(self.user_location)
+        self.user_lat = self.user_location[0]
+        self.user_long = self.user_location[1]
+
+    def get_location(self):
+        self.user_location = self.location_instance.latlng
+        self.user_lat = self.user_location[0]
+        self.user_long = self.user_location[1]
+
+    def get_user_lat(self):
+        return self.user_lat
+    
+    def get_user_long(self):
+        return self.user_long
